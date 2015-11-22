8fdb16d1c88417cd8bff818f6ac671dcbe523ba2
RandomTiger <RandomTiger@a8779ed9-6a69-4ec7-9bad-7ce55dc78dff>
2015-11-22 18:06:48 +0000

    Rework particle systems and provide an interface to the creepy cat systems.

---
+++ trunk/Assets/Scripts/Game/Particles/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour
+{
+    private ParticleSystemAccessor mParticleSystemAccessor;
+
+    public void Start()
+    {
+        mParticleSystemAccessor = GetComponent<ParticleSystemAccessor>();
+    }
+
+    public void Update()
+    {
+        if (mParticleSystemAccessor)
+        {
+            ParticleSystem particleSystem = mParticleSystemAccessor.GetParticleSystem();
+            if (particleSystem && !particleSystem.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ trunk/Assets/Scripts/Game/Particles/ParticleSystemWrapperCreepyCat.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Creepy cat particle systems need to have the following changes applied:
+// Start lifetime to
+// Play on awake: false
+// Emission: Rate: 0
+// Emission: + Bursts 0.00 1
+public class ParticleSystemWrapperCreepyCat : ParticleSystemAccessor
+{
+    private ParticleSystem mParticleSystem;
+    private float mSizeMult = 10.0f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        // Have to be set on Awake so they are applied when played (PlayOnAwake=true)
+        mParticleSystem = transform.GetChild(0).GetComponent<ParticleSystem>();
+        mParticleSystem.loop = false;
+        mParticleSystem.startLifetime = 2;
+        mParticleSystem.startSize = mSizeMult;
+    }
+
+    public override ParticleSystem GetParticleSystem()
+    {
+        return mParticleSystem;
+    }
+
+    // public abstract 
+    public override void SetSize(float size)
+    {
+        mParticleSystem.startSize = mSizeMult * size;
+    }
+}
