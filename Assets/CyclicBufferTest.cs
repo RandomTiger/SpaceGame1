c1f3ea542920c17cd3a62df7857459d637029de7
RandomTiger <RandomSnowTiger@gmail.com>
2016-09-04 11:54:36 +0100

    Add cyclic buffer

---
+++ Assets/CyclicBufferTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CyclicBufferTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CyclicBuffer<int> test = new CyclicBuffer<int>(9);
+
+        int val = 5;
+        for (int i = 0; i < 8; i++)
+        {
+            val++;
+            Debug.Log("CyclicBuffer in " + val);
+            test.Add(val);
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Debug.Log("CyclicBuffer out " + test.GetFirst());
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            val++;
+            Debug.Log("CyclicBuffer in " + val);
+            test.Add(val);
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Debug.Log("CyclicBuffer out " + test.GetFirst());
+        }
+    }
+}
---
+++ Assets/Scripts/System/DesignPatterns/CyclicBuffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class CyclicBuffer<T>
+{
+    public CyclicBuffer(int size)
+    {
+        m_List = new T[size];
+    }
+
+    public void Add(T element)
+    {
+        int newTail = (m_Tail + 1) % m_List.Length;
+
+        if(newTail == m_Head)
+        {
+            throw new Exception("Out of buffer");
+        }
+
+        m_List[m_Tail] = element;
+        m_Tail = newTail;
+    }
+
+    public T GetFirst()
+    {
+        if (m_Head == m_Tail)
+        {
+            return default(T);
+        }
+
+        T element = m_List[m_Head];
+        m_Head = (m_Head + 1) % m_List.Length;
+        return element;
+    }
+
+    private T[] m_List;
+    int m_Head;
+    int m_Tail;
+}
